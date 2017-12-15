34bbb59ba484a729557b4b12ee0908b849204d8e
Dragos Varovici <dragos.varovici@solium.com>
2017-12-15 09:07:35 -0700

    Added fake locale

---
+++ webapp/src/main/resources/public/js/app.js
@@ -0,0 +1,154 @@
+import $ from "jquery";
+import React from "react";
+import ReactDOM from "react-dom";
+import {Router, Route, IndexRoute, useRouterHistory} from "react-router";
+import { createHistory } from 'history'
+        import {Modal, Button} from "react-bootstrap";
+import {FormattedMessage, IntlProvider, addLocaleData} from "react-intl";
+import App from "./components/App";
+import BaseClient from "./sdk/BaseClient";
+import Main from "./components/Main";
+import Login from "./components/Login";
+import Workbench from "./components/workbench/Workbench";
+import Repositories from "./components/repositories/Repositories";
+import Drops from "./components/drops/Drops";
+import Settings from "./components/settings/Settings";
+import WorkbenchActions from "./actions/workbench/WorkbenchActions";
+import SearchConstants from "./utils/SearchConstants";
+import UrlHelper from "./utils/UrlHelper";
+import SearchParamsStore from "./stores/workbench/SearchParamsStore";
+
+// NOTE this way of adding locale data is only recommeneded if there are a few locales.
+// if there are more, we should load it dynamically using script tags
+// https://github.com/yahoo/react-intl/wiki#locale-data-in-browsers
+import en from 'react-intl/locale-data/en';
+import fr from 'react-intl/locale-data/fr';
+import be from 'react-intl/locale-data/be';
+import ko from 'react-intl/locale-data/ko';
+import ru from 'react-intl/locale-data/ru';
+import de from 'react-intl/locale-data/de';
+import es from 'react-intl/locale-data/es';
+import it from 'react-intl/locale-data/it';
+import ja from 'react-intl/locale-data/ja';
+import pt from 'react-intl/locale-data/pt';
+import zh from 'react-intl/locale-data/zh';
+import tr from 'react-intl/locale-data/tr';
+addLocaleData([...en, ...fr, ...be, ...ko, ...ru, ...de, ...es, ...it, ...ja, ...pt, ...zh, ...tr]);
+
+__webpack_public_path__ = CONTEXT_PATH + "/";
+
+const browserHistory = useRouterHistory(createHistory)({basename: CONTEXT_PATH});
+
+import(
+  /* webpackChunkName: "[request]", webpackMode: "lazy" */
+  `../../properties/${LOCALE}.properties`).then(messages => {
+    
+    if (!global.Intl) {
+        // NOTE: require.ensure would have been nice to use to do module loading
+        // but webpack doesn't support ES6 so after Babel transcompile,
+        // require.ensure is no longer available.
+        // https://webpack.github.io/docs/code-splitting.html#es6-modules
+        $.getScript(getUrlWithContextPath('/webjars/Intl.js/dist/Intl.min.js'), () => {
+            $.getScript(getUrlWithContextPath('/webjars/Intl.js/locale-data/jsonp/') + LOCALE + '.js', () => {
+                startApp(getMergedMessages(messages));
+            });
+        });
+    } else {
+        startApp(getMergedMessages(messages));
+    }
+});
+
+
+//TODO should implement merging logic as part of the build in a specific loader
+var enMessages = require(`../../properties/en.properties`);
+
+function getMergedMessages(messages) {
+    return messages = _.merge(enMessages, messages);
+}
+
+function startApp(messages) {
+    ReactDOM.render(
+            <IntlProvider locale={LOCALE} messages={messages}>
+                <Router history={browserHistory}>
+                    <Route component={Main}>
+                        <Route path="/" component={App}>
+                            <Route path="workbench" component={Workbench} onLeave={onLeaveWorkbench}/>
+                            <Route path="repositories" component={Repositories} />
+                            <Route path="project-requests" component={Drops}/>
+                            <Route path="settings" component={Settings}/>
+                            <IndexRoute component={Repositories}/>
+                        </Route>
+                        <Route path="login" component={Login}></Route>
+                    </Route>
+            
+                </Router>
+            </IntlProvider>
+            , document.getElementById("app")
+            );
+}
+
+/**
+ * When leaving the workbench, reset the search param so that when reloading the workbench will start from the
+ * default state (avoid flickr and stale data).
+ */
+function onLeaveWorkbench() {
+    WorkbenchActions.searchParamsChanged({
+        "changedParam": SearchConstants.UPDATE_ALL
+    });
+}
+
+function loadBasedOnLocation(location) {
+    if (location.pathname === '/workbench' && location.action === 'POP') {
+        WorkbenchActions.searchParamsChanged(SearchParamsStore.convertQueryToSearchParams(location.query));
+    }
+}
+
+/**
+ * Listen to history changes, when doing a POP for the workbench, initialize 
+ * the SearchParamStore from the query string
+ * For the first load the listener is not active, need to access the current
+ * location via getCurrentLocation
+ */
+let currentLocation = browserHistory.getCurrentLocation();
+
+loadBasedOnLocation(currentLocation);
+
+browserHistory.listen(location => {
+    loadBasedOnLocation(location);
+});
+
+/**
+ * Override handler to customise behavior
+ */
+BaseClient.authenticateHandler = function () {
+    let containerId = "unauthenticated-container";
+    $("body").append("<div id=\"" + containerId + "\" />");
+
+    function okOnClick() {
+        let pathNameStrippedLeadingSlash = location.pathname.substr(1, location.pathname.length);
+        let currentLocation = pathNameStrippedLeadingSlash + window.location.search;
+
+        window.location.href = UrlHelper.getUrlWithContextPath("/login?") + $.param({"showPage": currentLocation});
+    }
+
+    ReactDOM.render(
+            <IntlProvider locale={LOCALE} messages={MESSAGES}>
+                <Modal show={true}>
+                    <Modal.Header closeButton={true}>
+                        <Modal.Title>
+                            <FormattedMessage id="error.modal.header.title" />
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <FormattedMessage id="error.modal.message.loggedOut" />
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button bsStyle="primary" onClick={okOnClick}>
+                            <FormattedMessage id="label.okay" />
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </IntlProvider>
+            , document.getElementById(containerId));
+};
+
---
+++ webapp/src/main/resources/public/js/utils/Locales.js
@@ -0,0 +1,206 @@
+import Cldr from "cldrjs";
+import _ from "lodash";
+
+Cldr.load(require('cldr-data/supplemental/likelySubtags.json'));
+
+//TODO Load all locale data for now in the bundle... as number of locale grows we should only used locale data.
+Cldr.load(require('cldr-data/main/be/languages.json'));
+Cldr.load(require('cldr-data/main/be/territories.json'));
+
+Cldr.load(require('cldr-data/main/en/languages.json'));
+Cldr.load(require('cldr-data/main/en/territories.json'));
+
+Cldr.load(require('cldr-data/main/en-CA/languages.json'));
+Cldr.load(require('cldr-data/main/en-CA/territories.json'));
+
+Cldr.load(require('cldr-data/main/fr/languages.json'));
+Cldr.load(require('cldr-data/main/fr/territories.json'));
+
+Cldr.load(require('cldr-data/main/ko/languages.json'));
+Cldr.load(require('cldr-data/main/ko/territories.json'));
+
+Cldr.load(require('cldr-data/main/ru/languages.json'));
+Cldr.load(require('cldr-data/main/ru/territories.json'));
+
+Cldr.load(require('cldr-data/main/de/languages.json'));
+Cldr.load(require('cldr-data/main/de/territories.json'));
+
+Cldr.load(require('cldr-data/main/es/languages.json'));
+Cldr.load(require('cldr-data/main/es/territories.json'));
+
+Cldr.load(require('cldr-data/main/it/languages.json'));
+Cldr.load(require('cldr-data/main/it/territories.json'));
+
+Cldr.load(require('cldr-data/main/ja/languages.json'));
+Cldr.load(require('cldr-data/main/ja/territories.json'));
+
+Cldr.load(require('cldr-data/main/pt/languages.json'));
+Cldr.load(require('cldr-data/main/pt/territories.json'));
+
+Cldr.load(require('cldr-data/main/zh-Hans/languages.json'));
+Cldr.load(require('cldr-data/main/zh-Hans/territories.json'));
+
+Cldr.load(require('cldr-data/main/zh-Hant/languages.json'));
+Cldr.load(require('cldr-data/main/zh-Hant/territories.json'));
+
+Cldr.load(require('cldr-data/main/tr/languages.json'));
+Cldr.load(require('cldr-data/main/tr/territories.json'));
+
+class Locales {
+
+    constructor() {
+        //TODO don't use global var for LOCALE?
+        this.currentLocale = LOCALE;
+
+        this.cldr = new Cldr(this.currentLocale);
+    }
+
+    /**
+     * Gets the list of supported locales in the app.
+     *
+     * Must add CLDR data accordingly, see above.
+     *
+     * @returns {string[]}
+     */
+    getSupportedLocales() {
+        return [
+            'be',
+            'de',
+            'en',
+            'es',
+            'fr',
+            'it',
+            'ja',
+            'ko',
+            'pt',
+            'ru',
+            'zh-Hans',
+            'zh-Hant',
+            'tr'
+        ];
+    }
+
+    /**
+     * Gets a list of language that are RTL
+     * @returns {string[]}
+     */
+    getRightToLeftLanguages() {
+
+        return [
+            'ar',
+            'arc',
+            'bqi',
+            'ckb',
+            'dv',
+            'fa',
+            'glk',
+            'he',
+            'lrc',
+            'mzn',
+            'pnb',
+            'ps',
+            'sd',
+            'ug',
+            'ur',
+            'yi'
+        ];
+    }
+
+
+    /**
+     * Gets the current locale of the app as a bcp47 tag
+     *
+     * @returns {string} bcp47 tag of the app locale
+     */
+    getCurrentLocale() {
+        return this.currentLocale;
+    }
+
+
+    /**
+     * Gets the display name of the current locale
+     *
+     * @returns {string} the display name of the current locale
+     */
+    getCurrentLocaleDisplayName() {
+        return this.getDisplayName(this.getCurrentLocale());
+    }
+
+    /**
+     * Gets the locale display name in current language, format: language (territory)
+     *
+     * @param bcp47Tag the bcp47 tag
+     * @returns {string} the locale display name
+     */
+    getDisplayName(bcp47Tag) {
+
+        const targetCldr = new Cldr(bcp47Tag);
+
+        const language = targetCldr.attributes.language;
+        const territory = targetCldr.attributes.territory;
+
+        const languageDisplay = this.cldr.main("localeDisplayNames/languages/" + language);
+        const regionDisplay = this.cldr.main("localeDisplayNames/territories/" + territory);
+
+        return languageDisplay + ' (' + regionDisplay + ')';
+    }
+
+    /**
+     * Gets the native display name of a language.
+     * Eg. getNativeDispalyName('be') -> беларуская
+     *
+     * @param language language
+     * @returns {string} the native display name
+     */
+    getNativeDispalyName(language) {
+        const targetCldr = new Cldr(language);
+
+        const languageDisplay = targetCldr.main("localeDisplayNames/languages/" + language);
+
+        return languageDisplay;
+    }
+
+    /**
+     * Gets the direction (ltr or rtl) of a locale
+     *
+     * @param bcp47Tag bcp47 tag of the locale
+     * @returns {string} the direction (ltr or rtl)
+     */
+    getLanguageDirection(bcp47Tag) {
+
+        const targetCldr = new Cldr(bcp47Tag);
+        const language = targetCldr.attributes.language;
+
+        let dir = "ltr";
+
+        if (_.includes(this.getRightToLeftLanguages(), language)) {
+            dir = "rtl";
+        }
+
+        return dir;
+    }
+
+    /**
+     * Returns sorted objects by the locale display name
+     *
+     * @param {Object[]} objects array of object that contains locale
+     * @param {Function} getBcp45Tag function to retrieve bcp47Tag from the object
+     * @returns {Object[]} sorted objects
+     */
+    sortByDisplayName(objects, getBcp47Tag) {
+        return objects
+            .map(object => {
+                let bcp47Tag = getBcp47Tag(object);
+                object.localeDisplayName = this.getDisplayName(bcp47Tag);
+                return object;
+            })
+            .sort((a, b) => a.localeDisplayName.localeCompare(b.localeDisplayName));
+    }
+
+}
+;
+
+export default new Locales();
+
+
+
