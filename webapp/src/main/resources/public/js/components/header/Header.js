5e2c82625152c92ac7ae30507f9f690d25d9ce02
Dragos Varovici <dragos.varovici@solium.com>
2018-01-03 16:15:47 -0700

    DEV-11 - Pictures

--- webapp/src/main/resources/public/js/components/header/Header.js
+++ webapp/src/main/resources/public/js/components/header/Header.js
@@ -69,7 +69,7 @@ let Header = React.createClass({
                             }}}><NavItem><FormattedMessage id="header.repositories" /></NavItem></LinkContainer>
                     <LinkContainer to="/workbench"><NavItem><FormattedMessage id="header.workbench" /></NavItem></LinkContainer>
                     <LinkContainer to="/project-requests"><NavItem><FormattedMessage id="header.projectRequests" /></NavItem></LinkContainer>
-                    <LinkContainer to="/workbench"><NavItem><FormattedMessage id="header.expressions" /></NavItem></LinkContainer>
+                    <LinkContainer to="/workbench"><NavItem><FormattedMessage id="header.pictures" /></NavItem></LinkContainer>
 
                 </Nav>
                 <Nav pullRight={true}>
